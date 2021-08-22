a0c7dc0016314635b006069fb5f976b96e66ddcf
Anraku Honoka <rakuraku2563nonnon@gmail.com>
2021-08-22 17:06:36 +0900

    初期設定

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Http\Request;
+
+use App\Models\Test;
+
+class TestController extends Controller
+{
+    public function index(){
+
+        $values=Test::all();
+        $tests=DB::table('tests')->select('id')->get();
+        dd($tests);
+        return view('tests.test',compact('values'));
+    }
+}
